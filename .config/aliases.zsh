4051aba20bbc860df22d86c494e7fded01dfa267
ori <orisneh@gmail.com>
2023-05-16 02:10:27 +0300

    added aliases and starship config files

---
+++ .config/aliases.zsh
@@ -0,0 +1,13 @@
+alias v='nvim'
+alias vi='nvim'
+alias vim='nvim'
+
+alias l='ls -lAh'
+alias ll='ls -lAh'
+alias la='ls -lah'
+
+alias k='kubectl'
+alias kx='kubectx'
+alias pods='kubectl get pods --all-namespaces'
+alias wp='watch -n 0.1 kubectl get pods --all-namespaces'
+
