2fceb9dcd616d98881e4afe490c09e0a3e56e9f0
ori <orisneh@gmail.com>
2023-06-11 23:19:09 +0300

    Added namespace to kubernetes context in starship config

--- .config/aliases.zsh
+++ .config/aliases.zsh
@@ -1,9 +1,6 @@
 #!/bin/zsh
 alias i='printf "hostname: $(hostname)\nuser: $(whoami)\nhome: $HOME\nshell:$SHELL\n"'
 
-alias op='/mnt/c/Program\ Files/1Password\ CLI/op.exe'
-alias sz='source ~/.zshrc'
-
 alias sshj='ssh -J localhost:2222'
 
 alias v='nvim'
