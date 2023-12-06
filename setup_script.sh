be9d89d1ffb1484b0f413f6e1e9393e7b9a46d08
Ori Sne <orisne@greeneye.ag>
2023-12-06 22:42:12 +0800

    adjusted setup script for new files

--- setup_script.sh
+++ setup_script.sh
@@ -3,19 +3,19 @@
 
 # Determine which OS we're running on
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    echo "Linux detected"
+    echo "[OS] Linux detected"
     OS="linux"
     if wsl.exe --version > /dev/null 2>&1; then
-        echo "WSL detected"
+        echo "[OS] WSL detected"
         WSL=true
     else
         WSL=false
     fi
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-    echo "Mac OSX detected"
+    echo "[OS] MacOS detected"
     OS="mac"
 else
-    echo "OS not supported"
+    echo "[OS] Operating system not supported"
     OS="unknown"
     exit 1
 fi
@@ -35,31 +35,31 @@ ln -svf "$(pwd)"/.config/aliases.zsh "$HOME"/.config
 ln -svf "$(pwd)"/.config/functions.zsh "$HOME"/.config
 
 # Neovim config
-#ln -svf "$(pwd)/.config/nvim" "$HOME/.config" # OLD CONFIG
 git clone https://github.com/irohn/kickstart.nvim.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
 nvim --headless "+Lazy! sync" +qa
 
 
 # Alacritty config
+echo "[Alacritty] Setting up..."
 if [ $OS = "linux" ]; then
     if [ $WSL = "true" ]; then
         windows_username=$(powershell.exe -Command "Set-Location -Path C:\Users; cmd /c echo %username%")
         windows_username=$(echo $windows_username | tr -d '\r')
         windows_path="/mnt/c/Users/$windows_username/AppData/Roaming/alacritty"
-        echo "WSL detected, copying files manually into $windows_path"
+        echo "[Alacritty] WSL detected, copying files manually into $windows_path"
         cp -r "$(pwd)/.config/alacritty" "$windows_path"
-        echo "populating alacritty.yml..."
-        echo "import:\n  - $windows_path/default.yml\n  - $windows_path/windows.yml" | sed 's|/mnt/c|C:|g' > "$windows_path/alacritty.yml"
+        echo "[Alacritty] populating alacritty.yml..."
+        echo "[Alacritty] import:\n  - $windows_path/default.yml\n  - $windows_path/windows.yml" | sed 's|/mnt/c|C:|g' > "$windows_path/alacritty.yml"
     else
         ln -svf "$(pwd)"/.config/alacritty "$HOME"/.config
     fi
 elif [ $OS = "mac" ]; then
-    echo "MacOS detected, copying files manually"
+    echo "[Alacritty] MacOS detected, copying files manually"
     cp -r "$(pwd)"/.config/alacritty "$HOME"/.config/alacritty
-    echo "populating alacritty.yml..."
-    echo "import:\n  - $HOME/.config/alacritty/default.yml\n  - $HOME/.config/alacritty/mac.yml" > "$HOME"/.config/alacritty/alacritty.yml
+    echo "[Alacritty] populating alacritty.yml..."
+    echo "[Alacritty] import:\n  - $HOME/.config/alacritty/default.yml\n  - $HOME/.config/alacritty/mac.yml" > "$HOME"/.config/alacritty/alacritty.yml
 else
-    echo "OS not supported"
+    echo "[Alacritty] OS not supported"
 fi
 
 # symlinks for tmux config and plugins
@@ -69,3 +69,4 @@ ln -svf "$(pwd)"/.tmux "$HOME"
 # symlinks for home config files
 ln -svf "$(pwd)"/.zshrc "$HOME"
 ln -svf "$(pwd)"/.hushlogin "$HOME"
+
