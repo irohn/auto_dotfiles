d66976c8ad2be450ac86c2e0f05fc3eb09013b90
Ori Sne <orisneh@gmail.com>
2023-12-29 23:05:42 +0200

    Added starship install in dotfiles bin

--- bin/starship_install.sh
+++ bin/starship_install.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 curl -sS https://starship.rs/install.sh | sh
+
+# Make sure we eval starship in .zshrc
+if ! grep -q "starship init zsh" ~/.zshrc; then
+    echo 'eval "$(starship init zsh)"' >> ~/.zshrc
+fi
