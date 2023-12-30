d3f7f9b54a3fff92b2c55ddd49bba731b1271e4a
Ori Sne <orisneh@gmail.com>
2023-12-30 20:35:01 +0200

    changed nvim colorscheme to tokyonight

--- apps/nvim/lua/custom/plugins/colorscheme.lua
+++ apps/nvim/lua/custom/plugins/colorscheme.lua
@@ -1,14 +1,20 @@
 -- [[ Install and set custom color scheme ]]
 return {
   {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    config = function()
+      vim.cmd [[colorscheme tokyonight-night]]
+    end,
+  },
+  {
     "catppuccin/nvim",
     name = "catppuccin",
     lazy = false,
     priority = 1000,
     opts = {},
-    config = function()
-      vim.cmd [[colorscheme catppuccin-mocha]]
-    end,
   },
 }
 
